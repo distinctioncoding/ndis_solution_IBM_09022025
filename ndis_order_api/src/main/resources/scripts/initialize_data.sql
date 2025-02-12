102133bb8b4fbd0ecad1d817051f42383cf18bc9
Distinction Coding Pty Ltd <teaching@distinctioncoding.com.au>
2025-02-12 21:38:46 +1100

    123

--- ndis_order_api/src/main/resources/scripts/initialize_data.sql
+++ ndis_order_api/src/main/resources/scripts/initialize_data.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS ndis_db
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_unicode_ci;
+
 -- 创建 OrderStatus 表
 CREATE TABLE IF NOT EXISTS order_status (
                                             order_status_id BIGINT AUTO_INCREMENT PRIMARY KEY,

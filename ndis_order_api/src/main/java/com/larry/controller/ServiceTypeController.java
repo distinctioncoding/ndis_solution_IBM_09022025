61635549b24270a0efce33bae02e9ffb0799692a
Distinction Coding Pty Ltd <teaching@distinctioncoding.com.au>
2025-02-12 21:59:37 +1100

    123

--- ndis_order_api/src/main/java/com/larry/controller/ServiceTypeController.java
+++ ndis_order_api/src/main/java/com/larry/controller/ServiceTypeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/service-types")
+@CrossOrigin(origins = "*")
 public class ServiceTypeController {
 
     private final ServiceTypeService serviceTypeService;
